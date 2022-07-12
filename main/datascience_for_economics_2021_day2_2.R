11ca7755d288cf7076d0e0e2929a4ade666629d6
Toshifumi Kuroda <kuroda@tku.ac.jp>
2022-07-12 18:40:50 +0900

    reshape day2-2

--- main/datascience_for_economics_2021_day2_2.R
+++ main/datascience_for_economics_2021_day2_2.R
@@ -1,11 +1,19 @@
 library(pwr)
 
-pwr.p.test(h = ES.h(p1 = 0.75, p2 = 0.50),
-           sig.level = 0.05,
-           power = 0.80,
-           alternative = "greater")
-coin <- pwr.p.test(h = ES.h(p1 = 0.75, p2 = 0.50),
-                   sig.level = 0.05,
-                   power = 0.80,
-                   alternative = "greater")
+pwr::pwr.p.test(
+  h = pwr::ES.h(p1 = 0.75, p2 = 0.50),
+  sig.level = 0.05,
+  power = 0.80,
+  alternative = "greater"
+  )
+
+coin <- pwr::pwr.p.test(
+  h = pwr::ES.h(
+    p1 = 0.75, 
+    p2 = 0.50
+    ),
+  sig.level = 0.05,
+  power = 0.80,
+  alternative = "greater"
+  )
 plot(coin)
